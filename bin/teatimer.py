a7a6a6be6e066fc541db3f21065a5e76cd0e18f8
Sebastian Stammler <stammler@cbs.tu-darmstadt.de>
2016-07-06 20:30:02 +0200

    Add timer input dialog to tea timer python script

--- bin/teatimer.py
+++ bin/teatimer.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3
 # from http://www.janjachnik.com/python/tea/timer/2015/09/21/python-tea-timer/
+# argparse and Timer input extended by me, Sebastian
+import gi
+gi.require_version('Notify', '0.7')
 from gi.repository import Notify
 import sys
 import time
+import argparse
+import tkinter as tk
+from tkinter import simpledialog as sd
 
 notify_title = 'Tea time!'
 notify_body = 'Your tea is ready, go get it!'
 notify_icon = 'dialog-information'
 
-default_wait_time = 120
+### Parse arguments
+parser = argparse.ArgumentParser(description='Tea timer :)')
+parser.add_argument('time', type=float, default=120, nargs='?',
+        help='Time in seconds or minutes (see -m)')
+parser.add_argument('-m', '--minutes', action='store_true',
+        help='Switch whether time is given in minutes, not seconds')
+parser.add_argument('-d', '--dialog', action='store_true',
+        help='Show graphical dialog for time entry')
+args = parser.parse_args()
 
-def usage():
-    print("Usage: ",sys.argv[0],"<wait_duration_in_seconds>")
+wait_time = args.time
 
-if len(sys.argv)>2:
-    usage()
-    sys.exit()
+### Create time input dialogue if requested
+if args.dialog:
+    root = tk.Tk()
+    root.withdraw()
+    unit = 'minutes' if args.minutes else 'seconds'
+    wait_time = sd.askfloat('Tea Timer', 'Time in ' + unit + ':')
+    if not wait_time: exit(1)
 
-wait_time = default_wait_time
-if len(sys.argv)>1:
-    try:
-        wait_time = int(sys.argv[1])
-    except ValueError:
-        usage()
-        print("Duration must be integer")
-        sys.exit()
-
+# Adjust time to seconds
+if args.minutes:
+    wait_time *= 60
 time.sleep(wait_time)
 
 Notify.init("Tea timer")
