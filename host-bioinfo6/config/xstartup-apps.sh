c9b18a1a3bd35bada126f45eb63548eb02bd2051
Sebastian Stammler <stammler@cbs.tu-darmstadt.de>
2016-12-07 15:38:23 +0100

    [i3] Move startup applications to wm agnostic startup script, per host Switch to clipster clipboard manager

---
+++ host-bioinfo6/config/xstartup-apps.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# WM agnostic script to start up common applications
+clipster -d &
+redshift-gtk &
+thunderbird &
+pidgin &
+spotify &
---
+++ host-xpsepp/config/xstartup-apps.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# WM agnostic script to start up common applications
+clipster -d &
+nm-applet &
+redshift-gtk &
+thunderbird &
+pidgin &
+spotify &
