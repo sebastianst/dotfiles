dbfa6c7f8caf5ee53b7b7a3145ebd6288bf7bcf2
Sebastian Stammler <stammler@cbs.tu-darmstadt.de>
2018-04-10 17:17:21 +0200

    [surfingkeys] Add config

---
+++ surfingkeys.conf.js
@@ -0,0 +1,8 @@
+map("K", "R");
+map("J", "E");
+map("H", "S");
+map("R", "F");
+map("F", "C");
+map("L", "D");
+map("E", "B");
+addSearchAliasX('s', 'startpage', 'https://startpage.com/do/dsearch?query=');
